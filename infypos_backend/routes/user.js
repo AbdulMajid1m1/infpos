f4ef57e57a84e5324da95c56801fcb20d123d3b4
Shah Hussain <95882307+shhahhussain@users.noreply.github.com>
2024-07-29 23:11:37 +0500

    Added controller for siginin admin

--- infypos_backend/routes/user.js
+++ infypos_backend/routes/user.js
@@ -3,10 +3,11 @@ const router = Router();
 
 const controller = require("../controllers/user");
 
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.delete);
-router.get('/', controller.getUsers);
-router.get('/search', controller.searchUsers);
+router.post("/", controller.create);
+router.post("/admin-login", controller.loginAdmin);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.delete);
+router.get("/", controller.getUsers);
+router.get("/search", controller.searchUsers);
 
 module.exports = router;
