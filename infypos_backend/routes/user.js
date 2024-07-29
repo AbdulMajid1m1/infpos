9a14763e15cc94888a793df4b02e02b357c5e6a3
Shah Hussain <95882307+shhahhussain@users.noreply.github.com>
2024-07-29 23:00:12 +0500

    Added login controller for admin

--- infypos_backend/routes/user.js
+++ infypos_backend/routes/user.js
@@ -4,6 +4,7 @@ const router = Router();
 const controller = require("../controllers/user");
 
 router.post('/', controller.create);
+router.post("/admin-login", controller.loginAdmin);
 router.put('/:id', controller.update);
 router.delete('/:id', controller.delete);
 router.get('/', controller.getUsers);
